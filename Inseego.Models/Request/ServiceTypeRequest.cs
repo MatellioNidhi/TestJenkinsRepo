fd1e530a40210a5da3b4e26c228b60a030c2913c
Vineet Gupta <vineet.gupta@matellio.com>
2020-09-24 16:34:53 +0530

    Added [Required] annotation for request models

--- Inseego.Models/Request/ServiceTypeRequest.cs
+++ Inseego.Models/Request/ServiceTypeRequest.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Inseego.Models.Request
@@ -8,27 +9,32 @@ namespace Inseego.Models.Request
     public class ServiceTypeGetByIdRequest
     {
         [JsonProperty("id")]
+        [Required]
         public string[] id { get; set; }       
     }   
 
     public class ServiceTypeCreateRequest
     {
         [JsonProperty("serviceTypeName")]
+        [Required]
         public string ServiceTypeName { get; set; }
     }
 
     public class ServiceTypeUpdateRequest
     {
         [JsonProperty("id")]
+        [Required]
         public string id { get; set; }
 
         [JsonProperty("serviceType")]
+        [Required]
         public string ServiceTypeName { get; set; }
     }
 
     public class ServiceTypeDeleteRequest
     {
         [JsonProperty("id")]
+        [Required]
         public string id { get; set; }
     }
 }
